21dca2c5e82b8cb05673f8dd1e4d93e6618c72c5
Josh Cederstrom <joshcederstrom@gmail.com>
2025-05-07 23:31:14 +0000

    Add pplot_profile function.

--- diffusion-model.py
+++ diffusion-model.py
@@ -4,6 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def plot_profile(x, concentration, color="r"):
+    plt.figure()
+    plt.plot(x, concentration, color)
+    plt.xlabel("x")
+    plt.ylabel("C")
+    plt.title("Concentration profile")
+    
 D = 100
 Lx = 300
 
@@ -17,11 +24,7 @@ c_right = 0
 C[x <= (Lx / 2)] = c_left 
 C[x > (Lx / 2)] = c_right
 
-plt.figure()
-plt.plot(x, C, "r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial Profile")
+plot_profile(x, C)
 
 nt = 5000
 dt = 0.5 * dx ** 2 / D 
@@ -29,9 +32,4 @@ dt = 0.5 * dx ** 2 / D
 for t in range(0, nt):
     C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
 
-plt.figure()
-plt.plot(x, C, "r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial Profile")
-
+plot_profile(x, C, color="b")
