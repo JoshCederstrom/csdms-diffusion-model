c42f2512a904eadd88ae46afdea361a39971368b
Josh Cederstrom <joshcederstrom@gmail.com>
2025-05-08 17:32:18 +0000

    Factor script into functions

--- diffusion.py
+++ diffusion.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 def plot_profile(x, concentration, color="r"):
@@ -14,25 +15,45 @@ def plot_profile(x, concentration, color="r"):
 def calculate_stable_time_step(dx, diffusivity):
     return 0.5 * dx**2 / diffusivity
 
-D = 100
-Lx = 300
+def step_like(x, step_at=0):
+    y = np.empty_like(x, dtype=float)
 
-dx = 0.5
-x = np.arange(start=0, stop=Lx, step=dx)
-nx = len(x)
+    y[:step_at] = 0.0
+    y[step_at] = 0.5
+    y[step_at + 1 :] = 1.0
 
-C = np.zeros_like(x)
-c_left = 500
-c_right = 0
-C[x <= (Lx / 2)] = c_left 
-C[x > (Lx / 2)] = c_right
+    return y
 
-plot_profile(x, C)
+def new_profile(x, form="step"):
+    match form:
+        case "step":
+            return step_like(x, step_at=len(x) // 2)
+        case _:
+            raise ValueError(f"unknown profile type ({form!r})")
 
-nt = 5000
-dt = calculate_stable_time_step(dx, D)
+def run_diffusion_model():
+    D = 100
+    Lx = 300
+    
+    dx = 0.5
+    x = np.arange(start=0, stop=Lx, step=dx)
+    nx = len(x)
+    
+    C = new_profile(x=x, form="step")
+    
+    plot_profile(x, concentration=C)
+    
+    nt = 5000
+    dt = calculate_stable_time_step(dx, D)
+    
+    for t in range(0, nt):
+        C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
+    
+    plot_profile(x, C, color="b")
 
-for t in range(0, nt):
-    C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
+    return(C)
 
-plot_profile(x, C, color="b")
+
+if __name__ == "__main__":
+    cake = run_diffusion_model()
+    np.savetxt(sys.stdout, cake, fmt="%.6f")
